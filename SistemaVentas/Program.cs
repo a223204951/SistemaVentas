eff2ec09541e279b8f5778e9f42bfd3ba87b698e
a223204951 <a223204951@unison.mx>
2026-02-05 18:59:26 -0700

    PRUEBA DE COMENTARIO

--- SistemaVentas/Program.cs
+++ SistemaVentas/Program.cs
@@ -20,6 +20,7 @@ namespace SistemaVentas
             Application.SetCompatibleTextRenderingDefault(false);
             // Inicia la aplicación mostrando el formulario de listado de clientes
             Application.Run(new CapaPresentacion.FrmListadoCliente());
+            // PRUEBA COMENTARIO
         }
     }
 }
